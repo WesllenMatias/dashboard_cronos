d7b512367900bf9be26f236d595af4b79d38f06e
Wesllen Matias Maia <wesllen.maia2010@gmail.com>
2022-02-22 18:21:00 -0300

    Ajuste na saida do Valor de vendas, Configurando CSS...

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from http import server
 from flask import Flask, render_template
 import pymssql
 import consulta
+import locale
 
 
 
@@ -11,7 +12,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-
+    locale.setlocale(locale.LC_MONETARY,'pt_BR.UTF-8')
+    
     pesovendido = consulta.PesoVendido()
-    vendido = consulta.ValorVendido()
+    vd = consulta.ValorVendido()
+    vendido = locale.currency(vd, grouping=True)
     return render_template('index.html', peso=pesovendido,vendas=vendido)
+
+
+app.run(host="0.0.0.0",port=5000)
