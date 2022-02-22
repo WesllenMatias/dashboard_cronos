6f681fc1d76f1c7548f4337c8ac7f64a630a2e09
Wesllen Matias Maia <wesllen.maia2010@gmail.com>
2022-02-21 23:16:57 -0300

    Adicionando Flask na aplicação e configurando.

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from asyncio.base_events import Server
 from http import server
-from flask import Flask
+from flask import Flask, render_template
 import pymssql
 import consulta
 
 
-pesovendido = consulta.PesoVendido()
-vendido = consulta.ValorVendido()
-print(f'Peso total Vendido: {pesovendido} KG\nVenda Total: R$ {vendido}')
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+
+    pesovendido = consulta.PesoVendido()
+    vendido = consulta.ValorVendido()
+    return render_template('index.html', peso=pesovendido,vendas=vendido)
