c0fcad63817f6fe2ab5d37bf460aaab71adaa296
Wesllen Matias Maia <wesllen@pop-os.localdomain>
2022-02-23 07:57:27 -0300

    Alteraçoes na logica main.py e no template adicionando Canceladas Hoje e Taxa de Cancelamento

--- consulta.py
+++ consulta.py
@@ -7,7 +7,7 @@ def PesoVendido():
     conn = pymssql.connect(server=config.servidor, port=config.porta, 
                            database=config.db, user=config.user, password=config.passwd)
     sql = conn.cursor()
-    sql.execute("SELECT SUM(PesoLiquido) as PesoVendido FROM dbo.Movimento WHERE DtMov = CONVERT(date,GETDATE()) AND NotaFiscalImpressa='S'")
+    sql.execute("SELECT SUM(PesoLiquido) as PesoVendido FROM dbo.Movimento WHERE DtMov = CONVERT(date,GETDATE()) AND NotaFiscalImpressa='S' AND StatusOS != 'C'")
     row = sql.fetchone()
     peso = row[0]
     sql.close()
@@ -17,8 +17,32 @@ def ValorVendido():
     conn = pymssql.connect(server=config.servidor, port=config.porta, 
                            database=config.db, user=config.user, password=config.passwd)
     sql = conn.cursor()
-    sql.execute("SELECT SUM(TotMov) as TotalVendido FROM dbo.Movimento WHERE DtMov = CONVERT(DATE,GETDATE()) AND NotaFiscalImpressa = 'S';")
+    sql.execute("SELECT SUM(TotMov) FROM dbo.Movimento WHERE DtMov = CONVERT(DATE,GETDATE()) AND NotaFiscalImpressa = 'S' AND StatusOS != 'C';")
     row = sql.fetchone()
     vendas = row[0]
     sql.close()
     return vendas
+
+def QtdVendas():
+    conn = pymssql.connect(server=config.servidor, port=config.porta, 
+                           database=config.db, user=config.user, password=config.passwd)
+    sql = conn.cursor()
+    sql.execute("SELECT COUNT(StatusOS) FROM dbo.Movimento WHERE DtMov = CONVERT(DATE,GETDATE()) AND NotaFiscalImpressa = 'S' AND StatusOS != 'C';")
+    row = sql.fetchone()
+    qtd_vendas = row[0]
+    sql.close()
+
+    return qtd_vendas
+
+
+#SELECT COUNT(StatusOS) FROM dbo.Movimento WHERE DtMov = CONVERT(DATE, GETDATE()) AND NotaFiscalImpressa = 'S' AND StatusOS = 'C';
+def QtdCanceladas():
+    conn = pymssql.connect(server=config.servidor, port=config.porta, 
+                    database=config.db, user=config.user, password=config.passwd)
+    sql = conn.cursor()
+    sql.execute("SELECT COUNT(StatusOS) FROM dbo.Movimento WHERE DtMov = CONVERT(DATE,GETDATE()) AND NotaFiscalImpressa = 'S' AND StatusOS = 'C';")
+    row = sql.fetchone()
+    qtd_canceladas = row[0]
+    sql.close()
+    
+    return qtd_canceladas
--- main.py
+++ main.py
@@ -16,8 +16,31 @@ def home():
     
     pesovendido = consulta.PesoVendido()
     vd = consulta.ValorVendido()
+    if vd == None:
+        vd = 0
+    else:
+        vd = consulta.ValorVendido()
     vendido = locale.currency(vd, grouping=True)
-    return render_template('index.html', peso=pesovendido,vendas=vendido)
+    if vendido:
+        pass
+    else:
+        vendido = 0
+        
+    qtd_vendido = consulta.QtdVendas()
+    if qtd_vendido:
+        pass
+    else:
+        qtd_vendido = 0
 
+    qtd_canceladas = consulta.QtdCanceladas()
+    if qtd_vendido == 0:
+        qtd_canceladas = 0
+        perc_cancel = 0
+    else:
+        qtd_canceladas = consulta.QtdCanceladas()
+        perc_cancel = (qtd_canceladas * 100) / qtd_vendido
+        perc_cancel = round(perc_cancel,2)
+    return render_template('index.html', peso=pesovendido,vendas=vendido,nvendas=qtd_vendido, canceladas=qtd_canceladas
+                            ,percent_cancel = perc_cancel)
 
-app.run(host="0.0.0.0",port=5000)
+app.run(host="0.0.0.0",port=7000)
